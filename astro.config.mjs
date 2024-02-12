a66f5148c6cb0a5ee078869a0470ef5636c7e7f1
mixdor <miguel-avila112-@outlook.com>
2024-02-11 22:45:03 -0500

    Config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Mixdor.github.io',
-  base: '/circuitikz-generator-web',
+  base: '/circuitikz-generator.github.io',
 })
